7ce453b533c2f557141b0683fbc0f81952da9a58
batrlvla <batrlvla@WL311650>
2016-08-09 10:10:34 +0200

    Feature: Open on left click

--- src/VB.PowerManager/View/Menu.cs
+++ src/VB.PowerManager/View/Menu.cs
@@ -2,20 +2,34 @@
 {
     using System;
     using System.Drawing;
+    using System.Reflection;
     using System.Windows.Forms;
     using Properties;
 
     public partial class Menu : Form
     {
+        private NotifyIcon notifyIcon;
+
         public Menu()
         {
             InitializeComponent();
 
             var systemTrayMenu = CreateSystemTrayMenu();
 
-            var notifyIcon = CreateNotifyIcon();
+            notifyIcon = CreateNotifyIcon();
 
             SetIconAndShow(notifyIcon, systemTrayMenu);
+        }
+
+        private void NotifyIconMouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button.Equals(MouseButtons.Left))
+            {
+                var showContextMenu = typeof(NotifyIcon).GetMethod(
+                    "ShowContextMenu", 
+                    BindingFlags.Instance | BindingFlags.NonPublic);
+                showContextMenu.Invoke(notifyIcon, null);
+            }
         }
 
         private void SetIconAndShow(NotifyIcon notifyIcon, ContextMenu sysTrayMenu)
@@ -32,7 +46,14 @@
                 Icon = new Icon(Resources.Lightning, 48, 48)
             };
 
+            notifyIcon.MouseUp += NotifyIconMouseUp;
+
             return notifyIcon;
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            notifyIcon.MouseUp -= NotifyIconMouseUp;
         }
 
         private ContextMenu CreateSystemTrayMenu()
